1b3dcdd3993459ccdf8c6b503ef5cdcb6a0cd8a5
Matt Ellis <mjellis@ucdavis.edu>
2026-02-16 21:42:56 -0800

    feat: swallow patch

--- config.def.h
+++ config.def.h
@@ -10,6 +10,7 @@
 /* appearance */
 static unsigned int borderpx  = 3;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating = 0;     /* 1 means swallow floating windows by default */
 static const int rmaster	  = 0;		  /* 1 means master-area is initially on the right */
 static int showbar            = 1;        /* 0 means no bar */
 static int showtitle          = 1;        /* 0 means no title */
@@ -58,13 +59,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ TERMCLASS,  NULL,       NULL,       0,            0,           -1 },
-	{ "Nautilus", NULL,       NULL,       1 << 8,		0,			  2 },
-	{ "Slack",	  NULL,		  NULL,		  1 << 1,		0,			  1 },
-	{ TERMCLASS,  "spterm",   NULL,       SPTAG(0),     1,           -1 },
-	{ TERMCLASS,  "spcalc",   NULL,       SPTAG(1),     1,           -1 },
+	/* class      instance    title       tags mask     isfloating  isterminal	noswallow	monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,			0,			0,		 	-1 },
+	{ TERMCLASS,  NULL,       NULL,       0,            0,          1,			0,			-1 },
+	{ "Nautilus", NULL,       NULL,       1 << 8,		0,			0, 			0,			 2 },
+	{ "Slack",	  NULL,		  NULL,		  1 << 1,		0,			0, 			0, 			 1 },
+	{ TERMCLASS,  "spterm",   NULL,       SPTAG(0),     1,          1, 			0, 			-1 },
+	{ TERMCLASS,  "spcalc",   NULL,       SPTAG(1),     1,          1, 			0, 			-1 },
+	{ NULL,      NULL,		  "Event Tester", 0,        0,          0,          1,          -1 }, /* xev */
 };
 
 /* layout(s) */
